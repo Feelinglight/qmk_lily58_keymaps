91e37bf2d019a2a0eb8a561f0828c84a2e876818
feelinglight <kolpacc123@mail.ru>
2025-02-16 22:25:57 +0500

    Изменено комбо для переключения раскладки

--- druotoni/keymap.h
+++ druotoni/keymap.h
@@ -3,7 +3,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM MO3_combo[] = {KC_BSPC, KC_N, COMBO_END};
-const uint16_t PROGMEM alt_shift_combo[] = {KC_BSPC, KC_M, COMBO_END};
+const uint16_t PROGMEM alt_shift_combo[] = {KC_BSPC, MO(2), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(MO3_combo, MO(3)),
